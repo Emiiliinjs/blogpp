c451e1593aa7a07a638bf3210c0214221cc22dda
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-03-06 11:06:31 +0200

    nig

---
+++ views/blog/index.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Visi ieraksti</title>
+</head>
+<body>
+  <h1>Visi bloga ieraksti</h1>
+
+<ul>
+<?php 
+foreach ($posts as $post) {
+    ?> <li> <?= $post["content"]?></li> <?php
+}
+?>
+
+</ul>
+
+
+</body>
+</html>
